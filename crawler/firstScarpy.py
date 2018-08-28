08d096f534db4b984b65597680a63f73c17c5b54
Woong <1406493495@qq.com>
2018-08-28 22:17:08 +0800

    [Bug XXXX] add some note Cause: None Solution: None Fixed Version: None Branch: None

--- crawler/firstScarpy.py
+++ crawler/firstScarpy.py
@@ -2,8 +2,10 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
+    # 用来标识爬虫，该名字在一个项目必须是唯一的
     name = "quotes"
 
+    # 必须返回一个可迭代的列表（可以是列表，也可以是生成器），Scrapy会从这些请求开始抓取网页
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
@@ -12,6 +14,7 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    # 用于从网页文本中抓取相应内容，我们需要根据自己的需要重写该方法
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
